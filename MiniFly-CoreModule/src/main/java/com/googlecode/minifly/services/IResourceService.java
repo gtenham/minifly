4c8391b9cf8382f1b586cf9d03c706c14787ca4c
gerton.tenham@gmail.com <gerton.tenham@gmail.com@fa809a5b-17d0-a316-c567-f0c8ab834ba1>
2011-08-18 17:24:56 +0000

    - Added license comment - Added exception handling

--- MiniFly-CoreModule/src/main/java/com/googlecode/minifly/services/IResourceService.java
+++ MiniFly-CoreModule/src/main/java/com/googlecode/minifly/services/IResourceService.java
@@ -1,5 +1,17 @@
-/**
- * 
+/*
+ * Copyright 2011 Gerton ten Ham
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 package com.googlecode.minifly.services;
 
@@ -9,10 +21,21 @@ import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 
 /**
+ * Resource service interface
+ * 
  * @author Gerton
  *
  */
 public interface IResourceService {
 
+	/**
+	 * Write resource from request to the servlet output stream.
+	 * Use docroot as the basepath for the resource
+	 * 
+	 * @param String docroot
+	 * @param HttpServletRequest request
+	 * @param ServletOutputStream os
+	 * @throws IOException
+	 */
 	void writeResourceToStream(String docroot, HttpServletRequest request, ServletOutputStream os ) throws IOException;
 }
--- MiniFly-WebModule/src/main/java/com/googlecode/minifly/web/MiniflyServlet.java
+++ MiniFly-WebModule/src/main/java/com/googlecode/minifly/web/MiniflyServlet.java
@@ -1,5 +1,17 @@
-/**
- * 
+/*
+ * Copyright 2011 Gerton ten Ham
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 package com.googlecode.minifly.web;
 
@@ -13,23 +25,49 @@ import javax.servlet.http.HttpServletResponse;
 import com.googlecode.minifly.services.impl.ResourceService;
 
 /**
+ * The <code>MiniflyServlet</code> is the servlet which provide the resource
+ * as requested in the servletPath.
+ * 
+ * Example: http://localhost:8080/js/main.js will look for the resource <code>/js/main.js</code>
+ * 
+ * The servlet will look for a context-param named <code>x-minifly-doc-root</code>
+ * holding the document root for the requested resource.
+ * 
+ * Example web.xml:
+ * <servlet>
+ *  	<servlet-name>minifly</servlet-name>
+ *  	<servlet-class>com.googlecode.minifly.web.MiniflyServlet</servlet-class>
+ * </servlet>
+ * <servlet-mapping>
+ *  	<servlet-name>minifly</servlet-name>
+ *  	<url-pattern>/</url-pattern>
+ * </servlet-mapping>
+ *	
+ * <context-param>
+ *  	<param-name>x-minifly-doc-root</param-name>
+ *  	<param-value>D:/www/public</param-value>
+ * </context-param>
+ * 
  * @author Gerton
  *
  */
 public class MiniflyServlet extends HttpServlet {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 7986474589664415111L;
 
 	private String documentRoot;
 	
+	/* (non-Javadoc)
+	 * @see javax.servlet.GenericServlet#init(javax.servlet.ServletConfig)
+	 */
 	public void init(ServletConfig config) throws ServletException {
 	    super.init(config);
 	    documentRoot = getServletContext().getInitParameter("x-minifly-doc-root");
 	}
 	
+	/* (non-Javadoc)
+	 * @see javax.servlet.http.HttpServlet#doGet(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 		throws ServletException, IOException {
 		
@@ -39,6 +77,12 @@ public class MiniflyServlet extends HttpServlet {
 		}
 		
 		ResourceService rs = new ResourceService();
-		rs.writeResourceToStream(documentRoot, request, response.getOutputStream());
+		
+		try {
+			rs.writeResourceToStream(documentRoot, request, response.getOutputStream());
+		} catch (IOException e) {
+			response.sendError(HttpServletResponse.SC_NOT_FOUND);
+		}
+		
 	}
 }
