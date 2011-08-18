2dc4c7078ae797370a05c917e764829ff984672b
gerton.tenham@gmail.com <gerton.tenham@gmail.com@fa809a5b-17d0-a316-c567-f0c8ab834ba1>
2011-08-18 17:24:56 +0000

    - Added license comment - Added exception handling

--- trunk/MiniFly-CoreModule/src/main/java/com/googlecode/minifly/services/impl/ResourceService.java
+++ trunk/MiniFly-CoreModule/src/main/java/com/googlecode/minifly/services/impl/ResourceService.java
@@ -1,11 +1,24 @@
-/**
- * 
+/*
+ * Copyright 2011 Gerton ten Ham
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 package com.googlecode.minifly.services.impl;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
@@ -13,6 +26,8 @@ import javax.servlet.http.HttpServletRequest;
 import com.googlecode.minifly.services.IResourceService;
 
 /**
+ * Resource service implementation
+ * 
  * @author Gerton
  *
  */
@@ -24,14 +39,25 @@ public class ResourceService implements IResourceService {
 	public void writeResourceToStream(String docroot,
 			HttpServletRequest request, ServletOutputStream os) throws IOException {
 		
+		InputStream resourceInputStream;
 		String requestURI = request.getServletPath();
 		
-		String filename = docroot + requestURI;
-        File resource = new File(filename);
+		if (docroot != null) {
+			String filename = docroot + requestURI;
+			File resource = new File(filename);
 		
-		FileInputStream fileInputStream = new FileInputStream(resource);
+			resourceInputStream = new FileInputStream(resource);
+		} else {
+			// Get resource from servlet context
+			resourceInputStream = request.getServletContext().getResourceAsStream(requestURI);
+		}
+		
+		if (resourceInputStream == null) {
+			throw new IOException();
+		}
+		
 		int bytes;
-		while ((bytes = fileInputStream.read()) != -1) {
+		while ((bytes = resourceInputStream.read()) != -1) {
 			os.write(bytes);
 		}
 	}
